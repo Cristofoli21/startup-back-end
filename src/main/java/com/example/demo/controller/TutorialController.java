aab44d34290e3a48229c4ac3f453eccda782bbe0
Pablo Sena <pablosena.atla@gmail.com>
2021-07-28 10:40:15 -0300

    Testando com uma rota de '/'

--- src/main/java/com/example/demo/controller/TutorialController.java
+++ src/main/java/com/example/demo/controller/TutorialController.java
@@ -29,6 +29,26 @@ public class TutorialController {
     @Autowired
     TutorialRepository TutorialRepository;
 
+    @GetMapping("/")
+    public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false)String title){
+        try{
+            List<Tutorial> tutorials = new ArrayList<Tutorial>();
+
+            if(title == null)
+            TutorialRepository.findAll().forEach(tutorials::add);
+            else
+                TutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+            
+            if(tutorials.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @GetMapping("/tutorials")
     public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false)String title){
         try{
